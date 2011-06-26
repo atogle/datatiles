24e80c351eafbe6eb5b5155d643dec9546eb08e8
Aaron Ogle <atogle@roosevelt.local>
2011-06-25 23:14:40 -0700

    update tests for new class

--- scripts/tests.py
+++ scripts/tests.py
@@ -1,25 +1,19 @@
 import unittest
-import create_tiles
+import datatiles
+import json
+
+config = json.load(open("tile.yml"))
+dt = datatiles.DataTiles(config, "states")
 
 class TileMadnessTests (unittest.TestCase):
     def test_tile_bounds(self):
-        config = {"extent": {
-            "top": 12520000,
-            "left": -20030000,
-            "bottom": 2504000,
-            "right": -7514000
-        }}
-        
-        bounds = create_tiles.get_tile_bounds(config, 4)
+        bounds = dt.get_tile_bounds(4)
         self.assertEqual(bounds, (0, 12, 5, 8))
 
-
     def test_tile_bounds(self):
-        self.assertEqual(create_tiles.rgbify(1), (0, 0, 1))
-        self.assertEqual(create_tiles.rgbify(256), (0, 1, 0))
-        self.assertEqual(create_tiles.rgbify(65536), (1, 0, 0))
-        
-        
+        self.assertEqual(dt.rgbify(1), (0, 0, 1))
+        self.assertEqual(dt.rgbify(256), (0, 1, 0))
+        self.assertEqual(dt.rgbify(65536), (1, 0, 0))
         
 if __name__ == "__main__":
     unittest.main()
